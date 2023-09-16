f3dc2a2ae04ebc9d186c1f29fd673b6e2e3fbdf7
nashdev255 <yutori1107@gmail.com>
2023-09-16 11:21:44 +0900

    how to grid in grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,18 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <div className='p-1 max-w-5xl grid grid-cols-3 gap-1 mx-auto'>
-        <div className='py-3 rounded-lg bg-sky-500 text-white text-center'>A</div>
-        <div className='py-3 rounded-lg bg-sky-500 text-white text-center'>B</div>
-        <div className='py-3 rounded-lg bg-sky-500 text-white text-center'>C</div>
-        <div className='py-3 rounded-lg bg-sky-500 text-white text-center'>D</div>
-        <div className='py-3 rounded-lg bg-sky-500 text-white text-center'>E</div>
-        <div className='py-3 rounded-lg bg-sky-500 text-white text-center'>F</div>
+      <div className='flex flex-col'>
+        <div className='p-1'>
+          <div className='rounded-lg bg-sky-500 py-3 text-center text-white'>H</div>
+        </div>
+        <div className='mx-auto grid max-w-5xl grid-cols-3 gap-1 p-1 pt-0'>
+          <div className='rounded-lg bg-sky-500 py-3 text-center text-white'>1</div>
+          <div className='rounded-lg bg-sky-500 py-3 text-center text-white'>2</div>
+          <div className='rounded-lg bg-sky-500 py-3 text-center text-white'>3</div>
+          <div className='rounded-lg bg-sky-500 py-3 text-center text-white'>4</div>
+          <div className='rounded-lg bg-sky-500 py-3 text-center text-white'>5</div>
+          <div className='rounded-lg bg-sky-500 py-3 text-center text-white'>6</div>
+        </div>
       </div>
     </div>
   )
