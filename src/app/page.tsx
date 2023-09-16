c4a4f21af3e64f5285eae3c524df6dd5985cedaa
nashdev255 <yutori1107@gmail.com>
2023-09-16 10:51:43 +0900

    using mx-auto to centerize grid-container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,15 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <div className='p-1 max-w-5xl grid grid-cols-3 gap-1 mx-auto'>
+        <div className='py-3 rounded-lg bg-sky-500 text-white text-center'>A</div>
+        <div className='py-3 rounded-lg bg-sky-500 text-white text-center'>B</div>
+        <div className='py-3 rounded-lg bg-sky-500 text-white text-center'>C</div>
+        <div className='py-3 rounded-lg bg-sky-500 text-white text-center'>D</div>
+        <div className='py-3 rounded-lg bg-sky-500 text-white text-center'>E</div>
+        <div className='py-3 rounded-lg bg-sky-500 text-white text-center'>F</div>
+      </div>
+    </div>
   )
 }
